16c98d246aafefcd6616a4a23cde5f115d1f4faa
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-11-05 10:30:00 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
 import React, { Component } from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
+import List from './List.js'
+import Create from './Create.js'
 import './App.css';
-
-
-
-import fetch from 'superagent';
-
-
 export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+
+                    <Switch>
+                        <Route
+                            path="/"
+                            exact
+                            render={(routerProps) => <List {...routerProps} />}
+                        />
+                        <Route
+                            path="/create"
+                            exact
+                            render={(routerProps) => <Create {...routerProps} />}
+                        />
 
 
-  state = {
 
-    data: [],
+                    </Switch>
 
-  }
-
-  componentDidMount = async () => {
-    await this.fetchFood();
-  }
-
-
-
-
-  fetchFood = async () => {
-    let response = await fetch.get(`https://salty-earth-02045.herokuapp.com/food`);
-
-    this.setState({
-      data: response.body
-
-
-    });
-
-  }
-
-
-  render() {
-    return (
-
-      <div className='food'>
-        <p>My crappy React app</p>
-        {
-          this.state.data.map(food =>
-
-            <div className='food' key={food.id}>
-              <p> name: {food.name}</p>
-              <p> type: {food.type}</p>
-
-              <p> Flavor level: {food.flavor}</p>
-
+                </Router>
             </div>
-
-
-
-          )
-        }
-
-
-      </div>
-    )
-  }
+        )
+    }
 }
-
---
+++ src/Create.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import request from 'superagent';
+
+const user = {
+    userId: 1
+};
+
+export default class CreatePage extends Component {
+    state = {
+        types: []
+    }
+
+    componentDidMount = async () => {
+        const response = await request.get('https://salty-earth-02045.herokuapp.com/type');
+
+        this.setState({ types: response.body });
+    }
+
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const newFood = {
+
+            name: this.state.name,
+
+            flavor: this.state.flavor,
+            is_good: true,
+            type_id: this.state.typeId,
+            owner_id: user.userId
+        };
+        console.log(newFood)
+
+        await request
+            .post('https://salty-earth-02045.herokuapp.com/food/')
+            .send(newFood);
+
+
+        this.props.history.push('/');
+    }
+
+    handleChange = (e) => {
+        this.setState({ typeId: e.target.value });
+    }
+
+    render() {
+        return (
+            <div>
+                create a food
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name
+                        <input onChange={e => this.setState({ name: e.target.value })} type="text" />
+                    </label>
+                    <label>
+                        Flavor
+                        <input onChange={e => this.setState({ flavor: e.target.value })} type="number" />
+                    </label>
+                    <label>
+                        Is it good?
+                        <select onChange={e => this.setState({ is_good: e.target.value })}>
+                            <option value={true} > yes </option>
+                            <option value={false} > no </option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Type
+                        <select onChange={this.handleChange}>
+                            <option>select type</option>
+                            {
+                                this.state.types.map(type => <option key={type.id} value={type.id}>
+                                    {type.type}
+                                </option>)
+                            }
+                        </select>
+                    </label>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
